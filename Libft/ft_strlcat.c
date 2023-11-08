c3a2147f7a747c9e18a8e50ab74a71b6da7bfb83
Manon Escoda <escoda.manon@gmail.com>
2023-11-08 20:09:14 +0100

    Libft_ft_strlcat

--- Libft/ft_strlcat.c
+++ Libft/ft_strlcat.c
@@ -6,13 +6,50 @@
 /*   By: mescoda <escoda.manon@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 10:05:53 by mescoda           #+#    #+#             */
-/*   Updated: 2023/11/08 10:48:02 by mescoda          ###   ########.fr       */
+/*   Updated: 2023/11/08 20:07:34 by mescoda          ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include <stdio.h>
-#include <string.h>
+#include <strings.h>
 #include <stdlib.h>
+
+void *ft_memcpy(void *dest, const void *src, size_t n)
+{
+    unsigned char *idest;
+    const unsigned char   *isrc;
+    
+    idest = (unsigned char *)dest;
+    isrc = (const unsigned char *)src;
+    while (n-- > 0)
+        *idest++ = *isrc++;
+    return (dest);
+}
+
+void    *ft_memchr(void *s, int c, size_t n)
+{
+    unsigned char *ptr;
+    
+    ptr = (unsigned char *)s;
+    while (n-- > 0)
+    { 
+        if (*ptr == c)
+            return ((void *)ptr);
+        ptr ++;
+    }
+    return (0);
+}
+
+size_t ft_strnlen(char *s, size_t maxlen)
+{
+    char*  p;
+    
+    p = ft_memchr(s, 0, maxlen);
+    if (p == NULL)
+        return maxlen;
+    else
+        return (p - s);
+}
 
 size_t ft_strlen(const char *s)
 {
@@ -24,54 +61,30 @@ size_t ft_strlen(const char *s)
     return(i);
 }
 
-size_t ft_strlcat(char *dst, const char *src, size_t size)
+size_t  ft_strlcat(char *dst, const char *src, size_t size)
 {
-    char *d;
-    const char *s;
-    size_t dlen;
-    size_t n;
-
-    *d = *dst;
-    s = src;
-    n = size;
-    while (n-- != 0 && *d != '\0')
-        d++;
-    dlen = d - dst;
-    n = size - dlen;
+    size_t srclen;
+    size_t dstlen;
     
-    if (n == 0)
-        return (dlen - ft_strlen(s));
-
-    while (*s != '\0')
+    srclen = ft_strlen(src);
+    dstlen = ft_strnlen(dst, size);
+    if (dstlen == size) 
+        return (size + srclen);
+    if (srclen < size - dstlen)
+        ft_memcpy((dst + dstlen), src, (srclen + 1));
+    else
     {
-        if (n != 1)
-        {
-            *d++ = *s;
-            n--;
-        }
-        *s++;
+        ft_memcpy((dst + dstlen), src, (size - dstlen - 1));
+        dst[size - 1] = '\0';
     }
-    *d = '\0';
-    return (dlen + (s - src));
+    return (dstlen + srclen);
 }
 
-/*int	main(int ac, char **av)
+/*int	main()
 {
-    if (ac == 4)
-    {
-        size_t size = atoi(av[3]);
-        size_t ret = ft_strlcat(av[1], av[2], size);
-        //size_t ret2 = strlcat(av[1], av[2], size);
-        printf("My ft_strlcat : %ld\n", ret);
-        //printf("Real strlcat : %ld\n", ret2);
-    }
-    return 0;
-}
-
-int	main()
-{
-	char	destination[] = "Coucou";
-	const char	source[] = " les amis !!!!!!!";
-	long unsigned int 	nb = 11;
-	printf("%ld\n", ft_strlcat(destination, source, nb));
+	char	destination[] = "This is ";
+	const char	source[] = "a potentially long string";
+	long unsigned int 	nb = 16;
+	printf("My ft_strlcat : %ld\n", ft_strlcat(destination, source, nb));
+    printf("Real strlcat : 33");
 }*/
